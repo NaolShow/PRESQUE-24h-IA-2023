89f1f41794bcb58b736ce1e9bfe756ceca80da39
Loan Jouhannet <loan.jouhannet@protonmail.com>
2023-04-29 19:56:56 +0200

    fix: depth sonar

--- Network/NetworkClient.cs
+++ Network/NetworkClient.cs
@@ -306,8 +306,11 @@ namespace Network {
 
                 // Si le score sonnarisé est -1 c'est le mal ancien donc on sauvegarde la couche
                 if (score == -1) {
-                    Map.Depth = depth;
-                    Map.HasDetectedMal = true;
+                    // Si on a pas détecté le mal
+                    if (!Map.HasDetectedMal) {
+                        Map.Depth = depth;
+                        Map.HasDetectedMal = true;
+                    }
                 }
                 // Sinon on indique que la cellule est sonnarisée
                 else cell.Type = OreType.Sonarized;
