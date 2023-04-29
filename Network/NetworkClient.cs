d08ccef01747df48bf67339c9266037373dd2ad2
Loan Jouhannet <loan.jouhannet@protonmail.com>
2023-04-29 17:55:56 +0200

    fix: detected mal

--- Network/NetworkClient.cs
+++ Network/NetworkClient.cs
@@ -297,7 +297,10 @@ namespace Network {
                 cell.SetSonnarizedScore(score);
 
                 // Si le score sonnarisé est -1 c'est le mal ancien donc on sauvegarde la couche
-                if (score == -1) Map.Depth = depth;
+                if (score == -1) {
+                    Map.Depth = depth;
+                    Map.HasDetectedMal = true;
+                }
                 // Sinon on indique que la cellule est sonnarisée
                 else cell.Type = OreType.Sonarized;
 
